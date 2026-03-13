791cf5dfda113d20e6124745b8bc2241b7b4935a
Utkarsh Johari <utkarshjohari863@gmail.com>
2026-03-13 18:50:19 +0530

    Add problem details and fix syntax in twoSum function
    Added problem description, platform, link, and difficulty for clarity.

--- two-sum.js
+++ two-sum.js
@@ -1,10 +1,15 @@
-var twoSum = function(nums, target) {
-    let map = {};
+// Problem: Two Sum
+// Platform: LeetCode
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+
+var twoSum = function(nums , target) {
+    const map = {};
 
     for(let i = 0; i < nums.length; i++) {
         const num = nums[i];
-        if (target - nums in map) {
-            return [i, map[target - num]];
+        if(target - num in map) {
+            return[i, map[target - num]];
         }
         map[num] = i;
     }
