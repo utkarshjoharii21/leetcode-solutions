f61b38899b3f6bb8f0443b8e2f402ca158c8dd05
Utkarsh Johari <utkarshjohari863@gmail.com>
2026-03-13 21:06:20 +0530

    Add problem description for remove duplicates function
    Added problem statement and metadata for LeetCode challenge.

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -1,3 +1,7 @@
+// Problem: Remove Duplicates From Sorted Array
+// Platform: LeetCode
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Difficulty: Easy
 var removeDuplicatesSortedArray = function(nums) {
     let i = 1;
 
