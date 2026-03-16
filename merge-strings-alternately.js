6cb889c7eb4147329037ec02ff91cb1db649ef3e
YourName <your@email.com>
2026-03-16 21:48:30 +0530

    Added Merge Strings Alternately solution

---
+++ merge-strings-alternately.js
@@ -0,0 +1,8 @@
+var mergeStrings = function(word1, word2) {
+    let result = '';
+    for(let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if(i < word1.length) result += word1[i];
+        if(i < word2.length) result += word2[i];
+    }
+    return result;
+};
